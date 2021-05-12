da08aa3dd710efc0d5536d53ba76fc227eb33919
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-12 04:28:47 -0700

    initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from 'gatsby'
+import Navbar from "../components/navbar"
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Navbar />
+      <h1>Home</h1>
+    </>
+  )
 }
