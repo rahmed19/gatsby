f5122404201564469e02e418b94c101344a91488
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-15 19:26:22 -0700

    image fix in gatsby plugin

---
+++ src/examples/Images.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import img from '../images/2.jpg'
+import Image from 'gatsby-image'
+
+const getImages = graphql`
+{
+    fixed: file(relativePath: {eq: "3.jpg"}) {
+      childImageSharp {
+        fixed(width: 300, height: 400) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+    fluid: file(relativePath: {eq: "2.jpg"}) {
+      childImageSharp {
+        fluid {
+            ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }  
+`
+
+export default function Images() {
+
+    const data = useStaticQuery(getImages)
+    return (
+        <section className="images">
+            <article className="single-image">
+                <h3>basic image</h3>
+                <img src={img} width="100%" />
+            </article>
+            <article className="single-image">
+                <h3>fixed</h3>
+                <Image fixed={data.fixed.childImageSharp.fixed} />
+            </article>
+            <article className="single-image">
+                <h3>fluid</h3>
+            </article>
+        </section>
+    )
+}
---
+++ src/pages/images.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+import Images from '../examples/Images'
+
+export default function images() {
+    return (
+        <Layout>
+            <Images />
+        </Layout>
+    )
+}
