d464ff1c5d68f2b9b9a9002d424d5663939031ab
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-15 18:41:52 -0700

    image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,9 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
