eb3fbc0a3929c704a339b057f4f0908a778c2be0
rahmed19 <ray.ahmed.sec@gmail.com>
2021-05-17 02:12:15 -0700

    contentful setup

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -5,14 +5,14 @@ import Image from 'gatsby-image'
 
 const getImages = graphql`
 {
-    fixed: file(relativePath: {eq: "3.jpg"}) {
+    fixed: file(relativePath: {eq: "2.jpg"}) {
       childImageSharp {
         fixed(width: 300, height: 400) {
           ...GatsbyImageSharpFixed
         }
       }
     }
-    fluid: file(relativePath: {eq: "2.jpg"}) {
+    fluid: file(relativePath: {eq: "1.jpg"}) {
       childImageSharp {
         fluid {
             ...GatsbyImageSharpFluid
@@ -24,20 +24,21 @@ const getImages = graphql`
 
 export default function Images() {
 
-    const data = useStaticQuery(getImages)
-    return (
-        <section className="images">
-            <article className="single-image">
-                <h3>basic image</h3>
-                <img src={img} width="100%" />
-            </article>
-            <article className="single-image">
-                <h3>fixed</h3>
-                <Image fixed={data.fixed.childImageSharp.fixed} />
-            </article>
-            <article className="single-image">
-                <h3>fluid</h3>
-            </article>
-        </section>
-    )
+  const data = useStaticQuery(getImages)
+  return (
+    <section className="images">
+      <article className="single-image">
+        <h3>basic image</h3>
+        <img src={img} width="100%" />
+      </article>
+      <article className="single-image">
+        <h3>fixed</h3>
+        <Image fixed={data.fixed.childImageSharp.fixed} />
+      </article>
+      <article className="single-image">
+        <h3>fluid</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} />
+      </article>
+    </section>
+  )
 }
